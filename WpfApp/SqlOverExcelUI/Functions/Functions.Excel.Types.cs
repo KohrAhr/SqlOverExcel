0bebdf88b4df324af2bde3df033cbe7045f98ea1
ZAM <ZAM@1CLICK.LV>
2019-09-10 08:58:42 +0300

    1) References to Interop.Excel added; 2) Excel functions from console app added

---
+++ WpfApp/SqlOverExcelUI/Functions/Functions.Excel.Types.cs
@@ -0,0 +1,71 @@
+﻿using ExcelObject = Microsoft.Office.Interop.Excel;
+
+namespace SqlOverExcelUI.Functions
+{
+    public delegate void IteratorEvent(string value, int x, int y);
+
+    public enum WorksheerOrder
+    {
+        /// <summary>
+        ///     First
+        /// </summary>
+        woFirst = 0,
+
+        /// <summary>
+        ///     Last
+        /// </summary>
+        woLast = 1
+    }
+
+    /// <summary>
+    ///     Structure contain different kind of Excel objects 
+    ///     and 
+    ///     common information about Excel file (file name)
+    /// </summary>
+    public class ExcelFile
+    {
+        /// <summary>
+        ///     Excel file name
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        ///     Excel application
+        /// </summary>
+        protected internal ExcelObject.Application ExcelApp { set; get; } = null;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected internal ExcelObject.Workbooks Books { set; get; } = null;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected internal ExcelObject.Workbook Sheet { set; get; } = null;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected internal ExcelObject.Worksheet Worksheet { set; get; } = null;
+
+        /// <summary>
+        ///     Verify that Excel object was loaded and initialized successfully
+        /// </summary>
+        /// <returns></returns>
+        public bool IsInitialized()
+        {
+            return IsBookInitialized() && IsSheetInitialized();
+        }
+
+        public bool IsBookInitialized()
+        {
+            return ExcelApp != null && Books != null;
+        }
+
+        public bool IsSheetInitialized()
+        {
+            return Sheet != null;
+        }
+    }
+}
--- WpfApp/SqlOverExcelUI/ViewModels/MainWindowVM.cs
+++ WpfApp/SqlOverExcelUI/ViewModels/MainWindowVM.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using Lib.MVVM;
 using Microsoft.Win32;
+using SqlOverExcelUI.Functions;
 using SqlOverExcelUI.Models;
 
 namespace SqlOverExcelUI.ViewModels
@@ -13,7 +14,7 @@ namespace SqlOverExcelUI.ViewModels
     public class MainWindowVM
     {
         #region Commands definition
-        public ICommand RunCommand { get; set; }
+        public ICommand RunAnalyticsCommand { get; set; }
         public ICommand SelectFileCommand { get; set; }
         #endregion Commands definition
 
@@ -32,19 +33,33 @@ namespace SqlOverExcelUI.ViewModels
         private void InitData()
         {
             Model = new MainWindowModel();
-            Model.ExcelFileName = "<test>";
         }
 
         private void InitCommands()
         {
-            RunCommand = new RelayCommand(RunCommandProc);
+            RunAnalyticsCommand = new RelayCommand(RunAnalyticsProc);
             SelectFileCommand = new RelayCommand(SelectFileProc);
         }
 
         #region Commands implementation
-        private void RunCommandProc(object o)
+        private void RunAnalyticsProc(object o)
         {
+            using (ExcelCore excelIn = new ExcelCore(Model.ExcelFileName, "16.0"))
+            {
+                List<string> worksheets = excelIn.GetWorksheets();
 
+                //foreach (String name in worksheets)
+                //{
+                //    Console.WriteLine("\t\"{0}\"", name);
+
+                //    ExcelObject.Worksheet worksheet = excel.GetWorksheet(name);
+
+                //    Console.WriteLine("\tLast row with data: {0}; Last column with data: {1}\n",
+                //        excel.GetCountOfRows(worksheet),
+                //        excel.GetCountOfCols(worksheet)
+                //    );
+                //}
+            }
         }
 
         private void SelectFileProc(object o)
