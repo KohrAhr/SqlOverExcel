3b11761425c02ad808f702fed7b646f77570108e
ZAM <ZAM@1CLICK.LV>
2019-10-14 21:11:36 +0300

    Preparing for Save & Load  functionality

---
+++ WpfApp/SqlOverExcelUI/Models/MainWindowBaseModel.cs
@@ -0,0 +1,42 @@
+﻿using Lib.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlOverExcelUI.Models
+{
+    public class MainWindowBaseModel : PropertyChangedNotification
+    {
+        /// <summary>
+        ///     Excel file for proceed
+        /// </summary>
+        public string ExcelFileName
+        {
+            get => GetValue(() => ExcelFileName);
+            set => SetValue(() => ExcelFileName, value);
+        }
+
+        /// <summary>
+        ///     SQL query to run
+        /// </summary>
+        public string SqlQuery
+        {
+            get { return GetValue(() => SqlQuery); }
+            set { SetValue(() => SqlQuery, value); }
+        }
+
+        public bool HDR
+        {
+            get { return GetValue(() => HDR); }
+            set { SetValue(() => HDR, value); }
+        }
+
+        public string TextForSearch
+        {
+            get { return GetValue(() => TextForSearch); }
+            set { SetValue(() => TextForSearch, value); }
+        }
+    }
+}
--- WpfApp/SqlOverExcelUI/Models/MainWindowModel.cs
+++ WpfApp/SqlOverExcelUI/Models/MainWindowModel.cs
@@ -12,7 +12,7 @@ namespace SqlOverExcelUI.Models
     /// <summary>
     ///     Model for Main Window
     /// </summary>
-    public class MainWindowModel : PropertyChangedNotification
+    public class MainWindowModel : MainWindowBaseModel
     {
         /// <summary>
         ///     Constructor
@@ -33,15 +33,6 @@ namespace SqlOverExcelUI.Models
         }
 
         /// <summary>
-        ///     Excel file for proceed
-        /// </summary>
-        public string ExcelFileName
-        {
-            get => GetValue(() => ExcelFileName);
-            set => SetValue(() => ExcelFileName, value);
-        }
-
-        /// <summary>
         ///     Data table contain SQL query execution result
         /// </summary>
         public DataTable QueryResult
@@ -57,27 +48,6 @@ namespace SqlOverExcelUI.Models
         {
             get { return GetValue(() => WorksheetItems); }
             set { SetValue(() => WorksheetItems, value); }
-        }
-
-        /// <summary>
-        ///     SQL query to run
-        /// </summary>
-        public string SqlQuery
-        {
-            get { return GetValue(() => SqlQuery); }
-            set { SetValue(() => SqlQuery, value); }
-        }
-
-        public bool HDR
-        {
-            get { return GetValue(() => HDR); }
-            set { SetValue(() => HDR, value); }
-        }
-
-        public string TextForSearch
-        {
-            get { return GetValue(() => TextForSearch); }
-            set { SetValue(() => TextForSearch, value); }
         }
     }
 }
